1ed9ccc602e9c74eaa9f8a9f70e1992864e88d1e
vorayash <vorayash9028@gmail.com>
2022-03-02 21:41:11 +0530

    simple alarm

---
+++ alarm.js
@@ -0,0 +1,47 @@
+let userInput = document.getElementById("alarmTime");
+let submit = document.getElementById("submit");
+let now = new Date();
+now = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+userInput.value = now;
+let audio;
+
+
+
+const setAlarm = () => {
+    userTime = new Date(userInput.value);
+    now = new Date();
+    let timeout = userTime - now;
+    console.log(timeout);
+
+
+    if (timeout < 0) {
+        userInput.classList.add('is-invalid');
+        userInput.classList.remove('is-valid');
+
+    }
+    else {
+        userInput.classList.add('is-valid');
+        userInput.classList.remove('is-invalid');
+        audio = new Audio('clock.mp4');
+
+        setTimeout(() => {
+            audio.play();
+            document.getElementById("stopAlarm").disabled = false;
+
+        }, timeout);
+        setTimeout(() => {
+            audio.pause();
+            audio.currentTime = 0;
+            document.getElementById("stopAlarm").disabled = true;
+        }, 1000);
+    }
+
+}
+
+const stopAlarm = () => {
+    audio.pause();
+    document.getElementById("stopAlarm").disabled = true;
+    audio.currentTime = 0;
+}
+
+var today = new Date();
